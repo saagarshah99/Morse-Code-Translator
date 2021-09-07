481e4b02ba496c5c796f1306a5e2f9f0bb013611
saagarshah99 <saagarshahs@hotmail.com>
2021-09-07 21:43:16 +0100

    Tests for spaces, punctation and numbers now work

--- assets/scripts/translator.test.js
+++ assets/scripts/translator.test.js
@@ -4,33 +4,35 @@ import englishToMorseCode from "./translator";
 // TODO: improve test descriptions
 
 describe("englishToMorseCode() tests", () => {    
-    test("Should return ******", () => {
+    test("Should be able to translate basic words", () => {
         const result = englishToMorseCode("hi");
         const outcome = "******";
-        
         expect(result).toBe(outcome);
     })
     
-    test("Should return -*****--*-*- -*-----**-", () => {
+    test("Should be able to recognise spaces", () => {
         const result = englishToMorseCode("thank you");
         const outcome = "-*****--*-*- -*-----**-";
-        
         expect(result).toBe(outcome);
     })
     
-    test("Should return translation for punctuation also", () => {
+    test("Should be able to translate punctuation", () => {
         const result = englishToMorseCode("okay, thanks");
         const outcome = "----*-*--*----**-- -*****--*-*-***";
-        
+        expect(result).toBe(outcome);
+    })
+    
+    test("Should be able to translate numbers", () => {
+        const result = englishToMorseCode("123");
+        const outcome = "*----**---***--";
         expect(result).toBe(outcome);
     })
 
     // TODO: add test for number input
     
-    test("Should return <i>Invalid input</i>", () => {
+    test("Should be able to handle empty strings", () => {
         const result = englishToMorseCode("");
         const outcome = "<i>Invalid input</i>";
-        
         expect(result).toBe(outcome);
     })
 });
